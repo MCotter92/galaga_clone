ec3f6563a9f6ec014f49448659ca8df0fc0e4c5a
maccotter11 <122413834+maccotter11@users.noreply.github.com>
2026-05-19 11:57:45 -0400

    added SinePath, CosinePath, ZigZagPath, and ReverseZigZagPath

--- assets/enemy.py
+++ assets/enemy.py
@@ -1,24 +1,44 @@
 import pygame
 
+from assets import healthbar
 from assets.entity import Entity
 from assets.healthbar import HealthBar
+from assets.paths import StraightPath
 
 
 class Enemy(Entity):
     def __init__(
-        self, name, img, width, height, angle, max_health, current_health, coords
+        self,
+        name,
+        img,
+        width,
+        height,
+        angle,
+        max_health,
+        current_health,
+        coords,
+        speed,
+        start_x,
+        start_y=0,
+        path=None,
     ):
         super().__init__(name, img, width, height, angle)
         self.surf = pygame.surface.Surface((width, height))
         self.rect: pygame.Rect = self.surf.get_rect(topleft=coords)
+
         self.topleft = self.rect.topleft
         self.width = width
         self.height = height
         self.coords = coords
-        self.x_coord = coords[0]
-        self.y_coord = coords[1]
+        self.x_intercept = start_x
+        self.start_x = start_x
+        self.start_y = start_y
+        self.coords[0] = self.start_x
+        self.coords[1] = self.start_y
 
-        self.sprite_path = []
+        self.speed = speed
+
+        self.sprite_path = path or StraightPath()
 
         self.max_health = max_health
         self.current_health = current_health
@@ -32,15 +52,16 @@ class Enemy(Entity):
         self.last_hit_time = 0
 
     def update(self, window_width, window_height):
-        self.y_coord += 1
-        self.rect.topleft = (self.x_coord, self.y_coord)
+        self.coords[0] = self.sprite_path.path(self.x_intercept, self.coords[1])
+        self.coords[1] += self.speed
         self.healthbar.rect.bottomleft = (
-            self.rect.topleft[0],
-            self.rect.topleft[1] - 10,
+            self.coords[0],
+            self.coords[1] - 10,
         )
-        if self.y_coord > window_height + 25:
+
+        if self.coords[1] > window_height + 25:
             self.kill()
 
     def draw(self, surface):
-        surface.blit(self.img, (self.x_coord, self.y_coord))
+        surface.blit(self.img, (self.coords[0], self.coords[1]))
         self.healthbar.draw(surface)
--- todo.md
+++ todo.md
@@ -1,6 +1,9 @@
 # Next
-- [ ] add more interesting enemy paths. for example, sine or cosine waves down the screen. can i mix and match them?  
+- [x] add more interesting enemy paths. for example, sine or cosine waves down the screen. can i mix and match them?  
 
 # Later
 - [ ] I feel like the code base could use a reorg. Should main.py only include def main() and const declairations? Idk 
+- [ ] I think I need to clean up the enemy class attributes. They seem messy. I want to make sure that my naming conventions are similary between Enemy and Player
+- [ ] add more enemy types and paths that more closely match what is in Galaga
+- [ ] learn about Galaga's menu, HUD, level system
 
